13f5d5638cb3156d702bc501e342f8086a757add
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-10-15 23:22:14 +0200

    7. Making The Car Move

---
+++ Racing_game/Assets/Scripts/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.AddForce(new Vector3(0f, 0f, 100f));
+
+        transform.position = theRB.position;
+    }
+}
