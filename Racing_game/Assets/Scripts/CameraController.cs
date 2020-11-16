14e6c83472d5285ae6e40861903a6ff64569d702
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-11-16 23:18:30 +0100

    22. Top-Down Camera

---
+++ Racing_game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CarController target;
+    private Vector3 offsetDir;
+
+    public float minDistance, maxDistance;
+    private float acticeDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetDir = transform.position - target.transform.position;
+
+        acticeDistance = minDistance;
+
+        offsetDir.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acticeDistance = minDistance + ((maxDistance - minDistance) * (target.theRB.velocity.magnitude / target.maxSpeed));
+
+        transform.position = target.transform.position + (offsetDir * acticeDistance);
+    }
+}
