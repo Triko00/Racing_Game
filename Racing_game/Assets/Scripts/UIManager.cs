16bd7286447c8f7d84bf5468aefb1ab51c32682d
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-12-30 15:40:19 +0100

    59. Creating A Pause Menu

--- Racing_game/Assets/Scripts/UIManager.cs
+++ Racing_game/Assets/Scripts/UIManager.cs
@@ -9,7 +9,9 @@ public class UIManager : MonoBehaviour
 
     public TMP_Text lapCounterText, bestLapTimeText, currentLapTimeText, positionText, countdownText, goText, raceResultText;
 
-    public GameObject resultsScreen;
+    public GameObject resultsScreen, pauseScreen;
+
+    public bool isPaused;
 
     private void Awake()
     {
@@ -25,11 +27,45 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        isPaused = !isPaused;
+
+        pauseScreen.SetActive(isPaused);
+
+        if(isPaused)
+        {
+            Time.timeScale = 0f;
+
+            AudioListener.pause = true;
+            AudioListener.volume = 0;
+        } else
+        {
+            Time.timeScale = 1f;
+
+            AudioListener.pause = false;
+            AudioListener.volume = 1;
+        }
     }
 
     public void ExitRace()
     {
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        AudioListener.volume = 1;
+
         RaceManager.instance.ExitRace();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("*Gunclick* quit game *gunshot*");
     }
 }
