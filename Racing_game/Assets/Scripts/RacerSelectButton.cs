6745746e02e374cc9fb5b7006c663c1736cdf7ea
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2021-01-05 17:59:04 +0100

    66. Selecting Racers

---
+++ Racing_game/Assets/Scripts/RacerSelectButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RacerSelectButton : MonoBehaviour
+{
+    public Image racerImage;
+
+    public CarController racerToSet;
+
+    public void SelectRacer()
+    {
+        RaceInfoManager.instance.racerToUse = racerToSet;
+
+        MainMenu.instance.racerSelectImage.sprite = racerImage.sprite;
+
+        MainMenu.instance.CloseRacerSelect();
+    }
+}
